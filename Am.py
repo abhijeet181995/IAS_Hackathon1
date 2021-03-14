e174578c9b9fa3bb57b75ad239d67a85718b9cf4
Abhijeet Srivastava <abhijeet181995@gmail.com>
2021-03-14 17:04:09 +0530

    Add files via upload

---
+++ Am.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask ,request , render_template ,redirect , url_for 
+from file_validator import validate_app
+
+app = Flask(__name__)
+
+app.config['upload_extensions']=['zip','json']
+app.config['upload_path']='temp'
+UserList={
+
+}
+
+
+@app.route("/app_upload",methods=["GET","POST"])
+def app_upload():
+    if request.method=="GET":
+        return render_template('app_upload.html')
+    if request.method=="POST":
+        #print(request)
+        file_received=request.files['app_file']
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        save_path=os.path.join(app.config['upload_path'],file_received.filename)
+        file_received.save(save_path)
+        try:
+            is_valid=validate_app(save_path)
+        except:
+            return "Unable to process file"
+        if not is_valid:
+            return "Wrong Config File Format"
+        return "File successfully uploaded"
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        user = request.form['user']
+        passwd = request.form['pass']
+        if UserList[user] == passwd:
+            return redirect(url_for('app_upload'))
+        else:
+            return redirect("/")
+    return render_template("index.html")
+
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    if request.method=="POST":
+        user = request.form['user']
+        passwd = request.form['pass']
+        UserList[user] = passwd
+        return redirect("/")
+    return render_template("signup.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ file_validator.py
@@ -0,0 +1,105 @@
+import zipfile
+import os
+import shutil
+import json
+import jsonschema
+
+app_config_schema={
+    "type":"object",
+    "properties":{
+        "app_name":{
+            "type":"string"
+        },
+        "sensors":{
+            "type":"array",
+            "items":{
+                "type":"number"
+            }
+        },
+        "dependencies":{
+            "type":"array",
+            "items":{
+                "type":"string"
+            }
+        },
+        "scheduling":{
+            "type":"object",
+            "properties":{
+                "start_time":{
+                    "type":"number"
+                },
+                "end_time":{
+                    "type":"number"
+                },
+                "job_type":{
+                    "type":"string"
+                },
+            }
+        }
+    }
+}
+
+sensor_config_schema={
+    "type":"object",
+    "properties":{
+        "Sensors":{
+            "type":"array",
+            "items":{
+                "$ref":"#/$defs/sensor" 
+            }
+        }
+    },
+    "$defs":{
+        "sensor":{
+            "type":"object",
+            "required":["Sensor_id","Sensor_location","Sensor_type","Sensor_output_type","Sensor_output_rate"],
+            "properties":{
+                "Sensor_id":{
+                    "type":"number"
+                },
+                "Sensor_location":{
+                    "type":"string"
+                },
+                "Sensor_type":{
+                    "type":"string"
+                },
+                "Sensor_output_type":{
+                    "type":"string"
+                },
+                "Sensor_output_rate":{
+                    "type":"number"
+                },
+            }
+        }
+    },
+}
+
+app_repo='./app_repo/'
+
+def validate_sensor_config(json_path):
+    with open(json_path,'r') as fp:
+        sensor_json=json.load(fp)
+    jsonschema.validate(sensor_json,schema=sensor_config_schema)
+
+def validate_app(zip_path):
+    file_name=zip_path.split('/')[-1].split('.')[:-1]
+    file_name=''.join(file_name)
+
+    with zipfile.ZipFile(zip_path,'r') as zip:
+        if len(zip.infolist())>1:
+            zip.extractall('./temp/'+file_name+'/')
+        else:
+            zip.extractall('./temp/')
+    
+    if os.path.exists('./temp/'+file_name):
+        with open('./temp/'+file_name+'/app_config.json','r') as fp:
+            app_json=json.load(fp)
+        try:
+            jsonschema.validate(app_json,app_config_schema)
+            validate_sensor_config('./temp/'+file_name+'/sensor_config.json')
+        except:
+            shutil.rmtree('./temp/'+file_name)
+            return False
+        shutil.move('./temp/'+file_name,app_repo+file_name)
+        os.remove(zip_path)
+    return True
