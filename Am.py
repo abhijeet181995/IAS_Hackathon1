3155fa42a650c9aeb4bca73853880be9cf764760
Abhijeet Srivastava <abhijeet181995@gmail.com>
2021-03-14 12:20:17 +0530

    Add files via upload

---
+++ Am.py
@@ -0,0 +1,26 @@
+from flask import Flask ,request , render_template
+
+app = Flask(__name__)
+
+UserList={
+
+}
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        user = request.form['user']
+        passwd = request.form['pass']
+        UserList[user] = passwd
+    return render_template("index.html")
+
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    if request.method=="POST":
+        print(request)
+    
+    return render_template("signup.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
