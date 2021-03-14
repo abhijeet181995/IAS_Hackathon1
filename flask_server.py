68b3ff54bf2b8f5b995ff0969c4e6c3d4dd7d76f
shubh299 <shubham.singh@students.iiit.ac.in>
2021-03-14 12:19:15 +0530

    Added file upload page

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,24 @@
-from flask import Flask,render_template,request
+from flask import *
+import os
 
 app=Flask(__name__)
+
+app.config['upload_extensions']=['zip','json']
+app.config['upload_path']='temp'
+
+@app.route("/app_upload",methods=["GET","POST"])
+def app_upload():
+    if request.method=="GET":
+        return render_template('app_upload.html')
+    if request.method=="POST":
+        #print(request)
+        file_received=request.files['app_file']
+        print(file_received.filename)
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        save_path=os.path.join(app.config['upload_path'],file_received.filename)
+        file_received.save(save_path)
+    return "File successfully uploaded"
 
 @app.route("/login",methods=["GET","POST"])
 def login():
@@ -8,7 +26,7 @@ def login():
         return render_template('login.html')
     if request.method=="POST":
         print(request.form.to_dict())
-        return 'Check'
+        return redirect(url_for('app_upload'))
 
 @app.route("/")
 @app.route("/index")
